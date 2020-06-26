ea2b41ebc204129ed03eff8f9a24a5c590ed0c83
phithai <phithai289@gmail.com>
2020-06-26 10:48:46 +0700

    Sửa tên

--- src/SapXepNhiPhan_diemtrinh.cpp
+++ src/SapXepNhiPhan_diemtrinh.cpp
@@ -1,59 +1,58 @@
 #include <stdio.h>
-
 #define size 1000
-
-void doicho(int &a, int &b);
-void nhapMang(int A[], int &n);
-void xuatMang(int A[], int n);
-void sapXepTangDan(int A[], int n);
-void timKiemNhiPhan(int A[], int x, int left, int right);
-
+void doiCho(int &a, int &b);
+void nhapMang(int a[], int &n);
+void xuatMang(int a[], int n);
+void sapXepTangDan(int a[], int n);
+void timKiemNhiPhan(int a[], int x, int left, int right);
 int main() {
-	int A[size], n, x;
-	nhapMang(A, n);
-	sapXepTangDan(A, n);
+	int a[size], n, x;
+	nhapMang(a, n);
+	sapXepTangDan(a, n);
 	printf("Tang dan: ");
-	xuatMang(A, n);
+	xuatMang(a, n);
 	printf("Nhap x: ");
 	scanf("%d", &x);
-	timKiemNhiPhan(A, x, 0, n);
+	timKiemNhiPhan(a, x, 0, n);
 	return 0;
 }
-void nhapMang(int A[], int &n) {
+void nhapMang(int a[], int &n) {
+	printf("Nhap n:");
 	scanf("%d", &n);
+	printf("nhap mang: ");
 	for (int i=0; i<n; i++)
-		scanf("%d", &A[i]);
+		scanf("%d", &a[i]);
 }
-void xuatMang(int A[], int n) {
+void xuatMang(int a[], int n) {
 	for (int i=0; i<n; i++)
-		printf ("%d ", A[i]);
+		printf ("%d ", a[i]);
 	printf("\n");
 }
-void sapXepTangDan(int A[], int n) {
+void sapXepTangDan(int a[], int n) {
 	for(int i=0; i<n; i++)
 	{
 		for(int j=i+1; j<n; j++)
 		{
-			if(A[j] < A[i])
-				doicho(A[i], A[j]);
+			if(a[j] < a[i])
+				doicho(a[i], a[j]);
 		}
 	}
 }
-void doicho(int &a, int &b) {
+void doiCho(int &a, int &b) {
 	int temp = a;
 	a = b; 
 	b= temp;
 }
-void timKiemNhiPhan(int A[], int x, int left, int right) {
+void timKiemNhiPhan(int a[], int x, int left, int right) {
 	if (left >= right)
 		printf("Khong tim thay x!\n");
 	else {
 		int mid = (left + right)/2;
-		if (A[mid]== x)
+		if (a[mid]== x)
 			printf("Phan tu %d o vi tri %d\n",x,mid);
-		else if(x > A[mid])
-			timKiemNhiPhan(A, x, mid+1, right);
-		else if(x< A[mid])
-			timKiemNhiPhan(A, x, left, mid-1);
+		else if(x > a[mid])
+			timKiemNhiPhan(a, x, mid+1, right);
+		else if(x< a[mid])
+			timKiemNhiPhan(a, x, left, mid-1);
 	}
 }
