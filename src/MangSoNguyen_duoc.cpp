ecb5748abcff22fca1787c3a2b0b155d3ee3e568
phithai <phithai289@gmail.com>
2020-06-27 13:08:47 +0700

    # thêm hàm liên quan đến "dương"

--- src/MangSoNguyen_duoc.cpp
+++ src/MangSoNguyen_duoc.cpp
@@ -4,19 +4,22 @@
 void nhapMang(int *a, int n);
 void xuatMang(int *a, int n);
 int timMax(int *a, int n);
-void lietKeViTriMax(int *a, int n);
 int timMin(int *a, int n);
+int timMaxDuong(int *a, int n);
+void lietKeViTriMax(int *a, int n);
+void lietKeViTriMaxDuong(int *a, int n);
 void lietKeViTri(int *a, int n, int x);
 void lietKeViTriMin(int *a, int n);
 bool laSoNguyenTo(int n);
-void lietKeSoNguyenTo(int *a, int n);
 bool laMangDuong(int *a, int n);
 bool laMangAm(int *a, int n);
+void lietKeSoNguyenTo(int *a, int n);
 void kiemTraMangDuong(int *a, int n);
 void kiemTraMangAm(int *a, int n);
 void kiemTraMangLaCapSoCong(int *a, int n);
 bool laMangCapSoCong(int *a, int n);
 void sapXepTangDan(int *a, int n);
+int demSoDuong(int *a, int n);
 int main()
 {
     int *a, n;
@@ -26,6 +29,7 @@ int main()
     nhapMang(a, n);
     xuatMang(a, n);
     lietKeViTriMax(a, n);
+    lietKeViTriMaxDuong(a, n);
     lietKeViTriMin(a, n);
     lietKeSoNguyenTo(a, n);
     kiemTraMangDuong(a, n);
@@ -64,6 +68,42 @@ int timMax(int *a, int n)
     }
     return max;
 }
+int timMin(int *a, int n)
+{
+    int min = *a;
+    for (int i = 1; i < n; i++)
+    {
+        if (*(a + i) < min)
+        {
+            min = *(a + i);
+        }
+    }
+    return min;
+}
+int timMaxDuong(int *a, int n)
+{
+    int maxDuong = 0;
+    bool coSoDuong = false;
+    // B1: kiểm tra mảng có số dương hay không?
+    for (int i = 0; i < n; i++)
+    {
+        if (*(a + i) > 0)
+        {
+            coSoDuong = true;
+            break;
+        }
+    }
+    // B2: tìm kiếm max dương
+    if (!coSoDuong)
+    {
+        printf("Không có số dương trong dãy");
+    }
+    else
+    {
+        timMax(a, n);
+    }
+    return maxDuong;
+}
 void lietKeViTri(int *a, int n, int x)
 {
     for (int i = 0; i < n; i++)
@@ -81,17 +121,14 @@ void lietKeViTriMax(int *a, int n)
     lietKeViTri(a, n, max);
 }
 
-int timMin(int *a, int n)
+void lietKeViTriMaxDuong(int *a, int n)
 {
-    int min = *a;
-    for (int i = 1; i < n; i++)
-    {
-        if (*(a + i) < min)
-        {
-            min = *(a + i);
-        }
+    int maxduong = timMaxDuong(a, n);
+    if (maxduong > 0)
+    { //ví dụ: 1,2,3,4,5,...
+        printf("\nmax duong  = %d o cac vi tri: ", maxduong);
+        lietKeViTri(a, n, maxduong);
     }
-    return min;
 }
 void lietKeViTriMin(int *a, int n)
 {
@@ -137,8 +174,8 @@ void lietKeSoNguyenTo(int *a, int n)
         }
     }
 }
-bool laMangDuong(int *a, int n)
-{ // kiểm tra bằng thủ thuật đếm
+int demSoDuong(int *a, int n)
+{
     int dem = 0;
     for (int i = 0; i < n; i++)
     {
@@ -147,6 +184,13 @@ bool laMangDuong(int *a, int n)
             dem++;
         }
     }
+    return dem;
+}
+bool laMangDuong(int *a, int n)
+{   // kiểm tra bằng thủ thuật đếm
+    // Bước 1: Đếm số dương
+    int dem = demSoDuong(a, n);
+    // Bước 2: kiểm tra xem có toàn là số dương hay không?
     return dem == n;
 }
 bool laMangAm(int *a, int n)
