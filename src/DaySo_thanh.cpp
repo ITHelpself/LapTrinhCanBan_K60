4e611830631da58f41eedfe2ffa8c4237de534ec
phithai <phithai289@gmail.com>
2020-07-07 16:26:30 +0700

    # fix hàm

--- src/DaySo_thanh.cpp
+++ src/DaySo_thanh.cpp
@@ -2,6 +2,17 @@
 
 #include<stdio.h>
 #include<conio.h>
+void nhap(int n);
+void xuat(int n);
+int tinhF(long s);
+int main(){
+	int n;
+	long s=0;
+	nhap(n);
+	xuat(n);
+	tinhF(s);
+	 return 0;
+}
 
 void nhap(int n){
 	printf("\n nhap n: ");
@@ -14,18 +25,8 @@ void xuat(int n){
 
 int tinhF(long s){
     int n;
-	for(int i; i<n;i++){
+	for(int i; i<n;i++){// i là biến cục bộ
 	printf("\n tong cua day so co %d phan tu la %ld" ,n ,s);}
-}
-
-int main(){
-	int n;
-	int i=1;
-	long s=0;
-	nhap(n);
-	xuat(n);
-	tinhF(i,s);
-	 return 0;
 }
 
 // anh oi anh xem code giuo em vs.
