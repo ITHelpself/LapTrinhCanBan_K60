ebeff4c3078414aad9c007d5231e494162b4e8bb
phithai <phithai289@gmail.com>
2020-07-07 16:21:17 +0700

    #code ban đầu

---
+++ src/DaySo_thanh.cpp
@@ -0,0 +1,32 @@
+// tinh s= 1+2+3+...+n
+
+#include<stdio.h>
+#include<conio.h>
+
+void nhap(int n){
+	printf("\n nhap n: ");
+	scanf("%d",&n);
+}
+
+void xuat(int n){
+	printf("\n so phan tu n la: %d",n);
+}
+
+int tinhF(int i, long s){
+    int n;
+	for(i; i<n;i++){
+	printf("\n tong cua day so co %d phan tu la %ld" ,n ,s);}
+}
+
+int main(){
+	int n;
+	int i=1;
+	long s=0;
+	nhap(n);
+	xuat(n);
+	tinhF(i,s);
+	 return 0;
+}
+
+// anh oi anh xem code giuo em vs.
+// code no chay thi chay nhung ma k cho ra kq
