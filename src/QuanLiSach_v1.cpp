ffec5a4838858bbb8d09e9559c46b6c866fc52b1
phithai <phithai289@gmail.com>
2020-07-26 22:37:24 +0700

    # nhập danh sách sách

--- src/QuanLiSach_v1.cpp
+++ src/QuanLiSach_v1.cpp
@@ -3,7 +3,6 @@ typedef struct Sach{
     int id;
     char name[30];
 };
-void nhapSach(Sach *sach);// nhap 1 cuon sach
 void nhapSach(int *n, Sach *&sach);// nhap danh sach sach
 void xuatSach(int n, Sach sach[]);
 int main(){
@@ -11,10 +10,6 @@ int main(){
     int n;
     nhapSach(&n,sach);
     xuatSach(n,sach);
-}
-void nhapSach(Sach *sach){
-        printf("Nhap id: ");
-        scanf("%d",&(sach->id));
 }
 void nhapSach(int *n, Sach *&sach){
     printf("nhap n:");
